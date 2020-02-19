8b00f9132813676296426fe5d60895c1dbaa2f5e
Georgy Firsov <gfirsov007@gmail.com>
2020-02-19 14:32:47 +0300

    MSVC 19.16 supported

--- PodSerializer/Config.h
+++ PodSerializer/Config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #if defined(_MSC_VER)
-#   if _MSC_VER < 1920
-#       error PodSerializer library requires MSVC version 19.20 and above
+#   if _MSC_VER < 1916
+#       error PodSerializer library requires MSVC version 19.16 and above
 #       error Compilers with versions below have some bugs, that prevent using reflection.
 #   endif // _MSC_VER < 1922
 #elif __cplusplus < 201402L
